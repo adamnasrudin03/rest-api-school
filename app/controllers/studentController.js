94a1c1eef97644cb949ec4ac810b217e884d8aa7
Adam Nasrudin <adamnasrudin858@gmail.com>
2020-08-20 20:20:37 +0700

    add student controller

---
+++ app/controllers/studentController.js
@@ -0,0 +1,111 @@
+const db = require("../models");
+const Model = db.student;
+
+exports.addOne = (req, res) => {
+  if (
+    !req.body.name ||
+    !req.body.gender ||
+    !req.body.class ||
+    !req.body.address
+  ) {
+    res.status(400).send({
+      message: "Data cannot be empty!",
+    });
+    return null;
+  }
+
+  Model.create(req.body)
+    .then((data) => {
+      res.send({
+        message: "Create successfully",
+        data: data,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message,
+      });
+    });
+};
+
+exports.findAll = (req, res) => {
+  Model.findAll()
+    .then((data) => {
+      res.send({
+        message: "Find All successfully",
+        data: data,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message,
+      });
+    });
+};
+
+exports.findById = (req, res) => {
+  const id = req.params.id;
+
+  Model.findByPk(id)
+    .then((data) => {
+      if (data == null) {
+        res.status(404).send({
+          message: `data with id ${id}, not found`,
+        });
+      } else {
+        res.send({
+          message: "Find By Id successfully",
+          data: data,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message,
+      });
+    });
+};
+
+exports.updateById = (req, res) => {
+  const id = req.params.id;
+
+  Model.update(req.body, { where: { id: id } })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "Update Successfully",
+        });
+      } else {
+        res.status(404).send({
+          message: `data with id ${id}, not found`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message,
+      });
+    });
+};
+
+exports.deleteById = (req, res) => {
+  const id = req.params.id;
+
+  Model.destroy({ where: { id: id } })
+    .then((num) => {
+      if (num == 1) {
+        res.send({
+          message: "delete successfully",
+        });
+      } else {
+        res.status(404).send({
+          message: `data with id ${id}, not found`,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message,
+      });
+    });
+};

7a7526cb41a2829ba002c1886d79bdb3ed5e04c9
Adam Nasrudin <adamnasrudin858@gmail.com>
2020-08-20 14:29:18 +0700

    initial server.js

--- server.js
+++ server.js
@@ -1,38 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+const database = require("./app/models")
 
 const app = express();
+const PORT = process.env.PORT || 8082;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-var corsOptions = {
-    origin: "http://localhost:8082"
-  };
-  
-  app.use(cors(corsOptions));
-  
-  // parse requests of content-type - application/json
-  app.use(bodyParser.json());
-  
-  // parse requests of content-type - application/x-www-form-urlencoded
-  app.use(bodyParser.urlencoded({ extended: true }));
-  
-  
-  const dbModels = require("./app/models");
-  dbModels.sequelize.sync();
+database.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
 
-  dbModels.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.");
-  });
-
-  // simple route
-//   app.get("/", (req, res) => {
-//     res.json({ message: "Welcome to bezkoder application." });
-//   });
-  
-  require("./app/routes/biodataRouter")(app);
-  // set port, listen for requests
-  const PORT = process.env.PORT || 8083;
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-  });

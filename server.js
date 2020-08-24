5c04afead17743a3a8c7e5afb431192087123d8f
Adam Nasrudin <adamnasrudin858@gmail.com>
2020-08-24 18:06:17 +0700

    courses to lesson

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const database = require("./app/models");
 const studentRouter = require("./app/routers/studentRouter");
 const teacherRouter = require("./app/routers/teacherRouter");
-const coursesRouter = require("./app/routers/coursesRouter");
+const lessonRouter = require("./app/routers/lessonRouter");
 const scoreRouter = require("./app/routers/scoreRouter");
 
 const app = express();
@@ -16,11 +16,18 @@ app.listen(PORT, () => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 //Generated database
-database.sequelize.sync();
+database.sequelize.sync().then(() => {
+  console.log(" re-sync db.");
+});
+
+// database.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
 
 //routes
 app.use("/students", studentRouter);
 app.use("/teachers", teacherRouter);
-app.use("/courses", coursesRouter);
+app.use("/lessons", lessonRouter);
 app.use("/assessments", scoreRouter);

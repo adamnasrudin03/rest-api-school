17fefaf7e2ae01d978ccce730cf261e2f9eecb73
Adam Nasrudin <adamnasrudin858@gmail.com>
2020-09-15 21:27:22 +0700

    add verify Token JWT

--- app/routers/teacherRouter.js
+++ app/routers/teacherRouter.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const router = express.Router();
+
+const { authJwt } = require("../middlewares");
 const controller = require("../controllers/teacherController");
 
-router.post("/add", controller.addOne);
-router.get("/", controller.findAll);
-router.get("/:id", controller.findById);
-router.put("/update/:id", controller.updateById);
-router.delete("/delete/:id", controller.deleteById);
+router.post("/add", [authJwt.verifyToken, authJwt.isAdmin], controller.addOne);
+router.get("/", [authJwt.verifyToken], controller.findAll);
+router.get("/:id", [authJwt.verifyToken, authJwt.isAdmin], controller.findById);
+router.put(
+  "/update/:id",
+  [authJwt.verifyToken, authJwt.isAdmin],
+  controller.updateById
+);
+router.delete(
+  "/delete/:id",
+  [authJwt.verifyToken, authJwt.isAdmin],
+  controller.deleteById
+);
 
 module.exports = router;

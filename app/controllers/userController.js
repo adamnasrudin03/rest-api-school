366a3b972fec08d950ca0b8ba0ee94c588ee9d1c
Adam Nasrudin <adamnasrudin858@gmail.com>
2020-09-21 22:29:46 +0700

    add method update user by id

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -1,11 +1,69 @@
+const db = require("../models");
+const User = db.user;
+const Role = db.role;
+
+var bcrypt = require("bcryptjs");
+
+exports.updateById = (req, res) => {
+  const id = req.params.id;
+  User.findOne({
+    where: {
+      id: id,
+    },
+  })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "User Not found." });
+      }
+
+      var passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+
+      if (!passwordIsValid) {
+        return res.status(401).send({
+          message: "Invalid Password!",
+        });
+      }
+
+      const dataUpdate = {
+        username: req.body.username,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.newPassword, 8),
+      };
+
+      User.update(dataUpdate, { where: { id: id } })
+        .then((data) => {
+          if (data == 1) {
+            res.send({
+              message: `Update user by id is  ${id}, successfully`,
+            });
+          } else {
+            res.status(404).send({
+              message: `Data by id is  ${id}, not found`,
+            });
+          }
+        })
+        .catch((err) => {
+          res.status(500).send({
+            message: err.message,
+          });
+        });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
+};
+
 exports.allAccess = (req, res) => {
-    res.status(200).send("Public Content.");
-  };
-  
-  exports.userBoard = (req, res) => {
-    res.status(200).send("User Content.");
-  };
-  
-  exports.adminBoard = (req, res) => {
-    res.status(200).send("Admin Content.");
-  };
+  res.status(200).send("Public Content.");
+};
+
+exports.userBoard = (req, res) => {
+  res.status(200).send("User Content.");
+};
+
+exports.adminBoard = (req, res) => {
+  res.status(200).send("Admin Content.");
+};
--- app/routers/userRouter.js
+++ app/routers/userRouter.js
@@ -1,8 +1,8 @@
 const { authJwt } = require("../middlewares");
 const controller = require("../controllers/userController");
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+module.exports = function (app) {
+  app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
@@ -12,15 +12,13 @@ module.exports = function(app) {
 
   app.get("/api/test/all", controller.allAccess);
 
-  app.get(
-    "/api/test/user",
-    [authJwt.verifyToken],
-    controller.userBoard
-  );
+  app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
 
   app.get(
     "/api/test/admin",
     [authJwt.verifyToken, authJwt.isAdmin],
     controller.adminBoard
   );
+
+  app.put("/api/auth/update/:id", [authJwt.verifyToken], controller.updateById);
 };
